08049eb36730f239c419e419348c80d65dc07d00
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-12-13 16:48:52 +0200

    10.7

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredPrice: "",
+    enteredDate: "",
+  });
+
+  //console.log(userInput);
   const titleChangeHandler = (event) => {
-    console.log(event);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
+
+  const priceChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredPrice: event.target.value,
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: userInput.enteredTitle,
+      price: userInput.enteredPrice,
+      date: new Date(userInput.enteredDate),
+    };
+    //console.log(expenseData);
+    setUserInput({
+        enteredTitle: "",
+        enteredPrice: "",
+        enteredDate: "",
+      });
+  };
+  //const [enteredTitle, setEnteredTitle] = useState("");
+  //const [enteredPrice, setEnteredPrice] = useState("");
+  //const [enteredDate, setEnteredDate] = useState("");
+
+  //const titleChangeHandler = (event) => {
+  //  setEnteredTitle(event.target.value);
+  //};
+  //const priceChangeHandler = (event) => {
+  //  setEnteredPrice(event.target.value);
+  //};
+  //const dateChangeHandler = (event) => {
+  //  setEnteredDate(event.target.value);
+  //};
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" 
-          onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={userInput.enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={priceChangeHandler}
+            value={userInput.enteredPrice}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2024-11-12" max="2026-01-31" />
+          <input
+            type="date"
+            min="2024-11-12"
+            max="2026-01-31"
+            onChange={dateChangeHandler}
+            value={userInput.enteredDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -30,4 +97,4 @@ const ExpenseForm = (props) => {
 };
 
 export default ExpenseForm;
-//{}
+//{} [] ({})
