210fd7212ec8cf49fef82a7e11475efb84c6cb4a
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-12-13 15:25:05 +0200

    10. Sündmused ja nende handler-id

--- src/components/Expenses/ExpenceItem.js
+++ src/components/Expenses/ExpenceItem.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "./ExpenceItem.css";
 import ExpenceDate from "./ExpenceDate.js";
+import Card from "../UI/Card.js";
+import { useState } from "react";
 
 const ExpenceItem = (props) => {
+  const [priceChange, setPriceChange] = useState(props.data.price);
+
+  const clickHandler = () => {
+    //console.log("Clicked");
+    setPriceChange(props.data.title);
+    //console.log(priceChange);
+  };
+
   return (
     <div className="expense-item">
       <ExpenceDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
-        <div className="expense-item__price">{props.data.price}</div>
+        <div className="expense-item__price">{priceChange}</div>
       </div>
+      <button onClick={clickHandler}>Click Me</button>
     </div>
   );
 };
 
 export default ExpenceItem;
+//{}
