c9554cb45b9e12bf8322ceecfbded801e4cc7b11
Tanel Pauls <Tanel.Pauls@voco.ee>
2024-12-20 17:44:51 +0200

    16.7

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -10,6 +10,10 @@ const ExpenseForm = (props) => {
   const amountInputRef = useRef();
   const dateInputRef = useRef();
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
     const enteredTitle = titleInputRef.current.value;
@@ -27,7 +31,11 @@ const ExpenseForm = (props) => {
       enteredAmount.trim() === "" ||
       enteredDate.trim() === ""
     ) {
-      alert("Please fill out all fields before submitting.");
+      setError({
+        title: "Invalid input",
+        message:
+          "Please enter a valid title or amount or date (non-empty values)",
+      });
       return;
     }
 
@@ -42,40 +50,51 @@ const ExpenseForm = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" id="title" ref={titleInputRef} />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            id="amount"
-            ref={amountInputRef}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2024-11-12"
-            max="2026-12-31"
-            id="date"
-            ref={dateInputRef}
-          />
-        </div>
+    <Fragment>
+      {error && (
+        <Error
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <div>
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" id="title" ref={titleInputRef} />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                id="amount"
+                ref={amountInputRef}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2024-11-12"
+                max="2026-12-31"
+                id="date"
+                ref={dateInputRef}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button" onClick={cancelHandler}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
       </div>
-      <div className="new-expense__actions">
-        <button type="button" onClick={cancelHandler}>
-          Cancel
-        </button>
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    </Fragment>
   );
 };
 
