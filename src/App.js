535d27ffdb5a9e72a87cd50b3ad907e557bfea0d
Tanel Pauls <Tanel.Pauls@voco.ee>
2025-01-09 12:49:55 +0200

    18.5

--- src/App.js
+++ src/App.js
@@ -2,26 +2,17 @@ import React, { useState, useEffect } from "react";
 import Expences from "./components/Expenses/Expences.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
-const DUMMY_EXPENSES = [
-  { id: "id1", date: new Date(2023, 10, 22), title: "New Book", amount: 39.99 },
-  { id: "id2", date: new Date(2024, 11, 21), title: "Old Book", amount: 9.99 },
-  {
-    id: "id3",
-    date: new Date(2025, 11, 19),
-    title: "Just Book",
-    amount: 19.99,
-  },
-];
-
 const App = () => {
-  const [expenses, setExpenses] = useState(() => {
-    const expensesFromLS = JSON.parse(localStorage.getItem("expenses"));
-    return expensesFromLS || [];
-  });
+  const [expenses, setExpenses] = useState([]);
 
-  useEffect(() => {
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-  }, [expenses]);
+  fetch("http://localhost:3005/expenses")
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      setExpenses(responseData.expenses);
+    });
+  console.log(expenses);
 
   const addExpenseHandler = (expense) => {
     setExpenses((previousExpenses) => {
