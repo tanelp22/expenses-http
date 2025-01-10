18a2094490bc4a7473d8d4fa9ac6fd57876e670b
Tanel Pauls <Tanel.Pauls@voco.ee>
2025-01-10 10:37:57 +0200

    19 Get all expenses data from backend file, (intended typo in url)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Expences from "./components/Expenses/Expences.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
+import Error from "./components/UI/Error.js";
 
 const App = () => {
   const [isFetching, setIsFetching] = useState(false);
   const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     const getExpenses = async () => {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3005/expenses");
-      const responseData = await response.json();
-      setExpenses(responseData.expenses);
+      try {
+        const response = await fetch("http://localhost:3005/expensses");
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed fetchin data");
+        }
+        setExpenses(responseData.expenses);
+      } catch (error) {
+        setError({
+          title: "An error occured!",
+          message: "Failed fetching expenses data, plaeasy try again later.",
+        });
+        setShowError(true);
+      }
       setIsFetching(false);
     };
     getExpenses();
   }, []);
+  console.log(error);
+  const errorHandler = () => {
+    setError(null);
+    setShowError(false);
+  };
+
   useEffect(() => {
     console.log(expenses); // Logs the updated state whenever `expenses` changes
   }, [expenses]);
@@ -28,8 +48,15 @@ const App = () => {
 
   return (
     <div className="App">
+      {showError && (
+        <Error
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expences expenses={expenses} isLoading={isFetching}/>
+      <Expences expenses={expenses} isLoading={isFetching} />
     </div>
   );
 };
